004530ef81c9c540de202b4f6c7926efd575bd08
Iskender Mazhitov <isken.majitov1@gmail.com>
2021-11-12 14:07:28 +0600

    creating function to concatenate Server_socket and Client_socket and send response and request

---
+++ views.py
@@ -0,0 +1,8 @@
+def index():
+    with open('templates/index.html', 'r') as templates:
+        return templates.read()
+    
+
+def blog():
+    with open('templates/index.html', 'r') as templates:
+        return templates.read()
